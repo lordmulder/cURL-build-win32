af142ed16c0b773d8b0e4910fd2b82ae5650c375
LoRd_MuldeR <mulder2@gmx.de>
2021-06-08 23:59:08 +0200

    Rename CA bundle file.

--- build.sh
+++ build.sh
@@ -166,7 +166,7 @@ pushd "${CURL_DIR}"
 patch -p1 -b < "${BASE_DIR}/patch/curl_findw32cacert.diff"
 patch -p1 -b < "${BASE_DIR}/patch/curl_mutex_init.diff"
 patch -p1 -b < "${BASE_DIR}/patch/curl_parseconfig.diff"
-CFLAGS="-march=${MY_MARCH} -mtune=${MY_MTUNE} -I\"${LIBS_DIR}/include\"" CPPFLAGS="-DNGHTTP2_STATICLIB" LDFLAGS="-static -no-pthread -L\"${LIBS_DIR}/lib\"" LIBS="-latomic -liconv -lcrypt32" PKG_CONFIG_PATH="${LIBS_DIR}/pkgconfig" ./configure --enable-static --disable-shared --disable-pthreads --disable-libcurl-option --with-zlib="${LIBS_DIR}" --with-zstd="${LIBS_DIR}" --with-brotli="${LIBS_DIR}" --with-openssl="${LIBS_DIR}" --with-libssh2="${LIBS_DIR}" --with-nghttp2="${LIBS_DIR}" --with-libidn2="${LIBS_DIR}" --with-ca-bundle="cacert.pem"
+CFLAGS="-march=${MY_MARCH} -mtune=${MY_MTUNE} -I\"${LIBS_DIR}/include\"" CPPFLAGS="-DNGHTTP2_STATICLIB" LDFLAGS="-static -no-pthread -L\"${LIBS_DIR}/lib\"" LIBS="-latomic -liconv -lcrypt32" PKG_CONFIG_PATH="${LIBS_DIR}/pkgconfig" ./configure --enable-static --disable-shared --disable-pthreads --disable-libcurl-option --with-zlib="${LIBS_DIR}" --with-zstd="${LIBS_DIR}" --with-brotli="${LIBS_DIR}" --with-openssl="${LIBS_DIR}" --with-libssh2="${LIBS_DIR}" --with-nghttp2="${LIBS_DIR}" --with-libidn2="${LIBS_DIR}" --with-ca-bundle="ca-bundle.pem"
 make curl_LDFLAGS=-all-static
 strip -s src/curl.exe
 popd
@@ -179,7 +179,7 @@ readonly OUT_DIR="${BASE_DIR}/.bin"
 rm -rf "${OUT_DIR}" && mkdir "${OUT_DIR}"
 pushd "${OUT_DIR}"
 cp -vf "${CURL_DIR}/src/curl.exe" curl.exe
-cp -vf "${LIBS_DIR}/cacert.pem"   cacert.pem
+cp -vf "${LIBS_DIR}/cacert.pem"   ca-bundle.pem
 cp -vf "${LIBS_DIR}/manpage.html" manpage.html
 mkdir -p "${OUT_DIR}/legal"
 unix2dos -n "${CURL_DIR}/COPYING"    legal/curl.COPYING.txt
