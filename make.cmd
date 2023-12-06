612ca15de9cfe7463f726eea3f15374929225617
LoRd_MuldeR <mulder2@gmx.de>
2023-12-06 23:29:42 +0100

    Updated cURL to version 8.5.0 (2023-12-06).

--- make.cmd
+++ make.cmd
@@ -6,7 +6,7 @@ set "MSYS2_DIR=C:\msys64"
 echo "%MSYS2_DIR%\msys2_shell.cmd" -mingw32 -where "%~dp0" -c "./build.sh"
 call "%MSYS2_DIR%\msys2_shell.cmd" -mingw32 -where "%~dp0" -c "./build.sh"
 
-TIMEOUT /T 10 /NOBREAK > NUL
+TIMEOUT /T 30 /NOBREAK > NUL
 
 echo "%MSYS2_DIR%\msys2_shell.cmd" -mingw64 -where "%~dp0" -c "./build.sh"
 call "%MSYS2_DIR%\msys2_shell.cmd" -mingw64 -where "%~dp0" -c "./build.sh"
